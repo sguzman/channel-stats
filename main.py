c88a97ca52cacbc2581b61338033edfc4e4eac0e
Salvador Guzman <guzmansalv@gmail.com>
2021-03-05 06:09:23 -0800

    working grpc server

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-import requests
 from bs4 import BeautifulSoup
+import grpc
+import json
+import logging
+import multiprocessing
+import multiprocessing.pool
+import requests
+import server_pb2
+import server_pb2_grpc
 
 
 init_str = 'var ytInitialData = '
@@ -27,8 +33,7 @@ def get_view(js):
 
     tmp = js
     for i in indexes:
-       tmp = tmp[i]
-
+        tmp = tmp[i]
     
     tmp = tmp.removesuffix(' views')
     tmp = tmp.replace(',', '')
@@ -67,15 +72,32 @@ def get(url):
     return doc
 
 
-def main():
-    g = get('UCL7DDQWP6x7wy0O6L5ZIgxg')
+def grab(arg):
+    g = get(arg)
     src = get_script(g)
     js = get_json(src)
     view = get_view(js)
 
-    print(json.dumps(view, indent=4, sort_keys=True))
+    return view
 
 
+def main():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
 
-if __name__ == "__main__":
+    server = grpc.server(multiprocessing.pool.ThreadPool(1))
+    server_pb2_grpc.add_ViewsServicer_to_server(server_pb2_grpc.ViewsServicer(), server)
+    server.add_insecure_port('[::]:9090')
+    server.start()
+    logging.info('Started server')
+    server.wait_for_termination()
+
+
+class Server(server_pb2_grpc.ViewsServicer):
+    def view(self, request, context):
+        name = grab(request.name)
+        return server_pb2.ChannelViews(name=name)
+
+
+if __name__ == '__main__':
     main()
